5b4602ff650ac782750ff9e0b139dc7704ff546d
adityanegara <aditnegarabisnis@gmail.com>
2021-08-23 13:51:03 +0700

    Configurig Webpack

---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+module.exports = {
+     entry : {
+         main : path.resolve(__dirname, 'src/scripts/index.js'),
+     },
+     plugins : [
+         new HtmlWebpackPlugin({
+         template : path.resolve(__dirname, 'src/templates/index.html')
+         }),
+         new CopyWebpackPlugin({
+          patterns: [
+              {
+                  from: path.resolve(__dirname, 'src/public'),
+                  to: path.resolve(__dirname, 'dist/')
+              }
+          ]
+         }),
+    ]
+}
