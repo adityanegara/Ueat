a9b440ee261cee69d29f61f1cc2ab2b120890b99
adityanegara <aditnegarabisnis@gmail.com>
2021-08-23 15:48:23 +0700

    Finish navbar

---
+++ src/scripts/component/navbar/navbar-links.js
@@ -0,0 +1,22 @@
+const links = [
+  {
+    id: 1,
+    href: '#/about',
+    caption: 'About',
+    tabindex: 0,
+  },
+  {
+    id: 2,
+    href: '#/explore',
+    caption: 'Explore',
+    tabindex: 0,
+  },
+  {
+    id: 3,
+    href: '#/favorite',
+    caption: 'Favorite',
+    tabindex: 0,
+  },
+];
+
+export default links;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,14 @@
-import "../styles/style.scss"
-alert("Index Js");
+import "../styles/main.scss"
+import './component/navbar/navbar-list';
+import links from './component/navbar/navbar-links';
+import navSlide from './utils/navbar-animation';
+
+const renderNavbar = (navbarLinks) => {
+    const navbarContainer = document.getElementById('nav-container');
+    const navbarListElement = document.createElement('navbar-list');
+    navbarListElement.navbarLinks = navbarLinks;
+    navbarContainer.appendChild(navbarListElement);
+};
+  
+renderNavbar(links);
+navSlide();
