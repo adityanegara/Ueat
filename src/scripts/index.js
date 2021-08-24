9eb2b43ee5708b029a7a63d5664ac5da8728d84d
adityanegara <aditnegarabisnis@gmail.com>
2021-08-24 17:29:44 +0700

    Finish Home Page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,3 +12,30 @@ const renderNavbar = (navbarLinks) => {
   
 renderNavbar(links);
 navSlide();
+
+const nav = document.querySelector('navbar-list');
+const logo = document.querySelector('.logo');
+const timeline = gsap.timeline();
+var x = window.matchMedia("(max-width: 768px)")
+if(x.matches){
+    console.log('768px');
+    timeline.fromTo(logo, 1, {opacity: "0%"}, {opacity: "100%",  ease : "power1"})
+    gsap.fromTo('#navbar-button', 1, { opacity : "0%"}, {opacity: "100%"});
+    timeline.from('#hero-section', 0.5 , {backgroundColor : "white"});
+    timeline.fromTo('.hero-image', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.hero-headline', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.hero-description', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.download-button', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    gsap.fromTo('.copyright', 1, {opacity: "0%"}, {opacity: "100%"});
+}else{
+    console.log('else');
+    timeline.fromTo(logo, 1, {opacity: "0%"}, {opacity: "100%",  ease : "power1"})
+    gsap.fromTo('#navbar-button', 1, { opacity : "0%"}, {opacity: "100%"});
+    timeline.fromTo('navbar-item', 0.5, {opacity: "0%"}, {opacity: "100%",  ease : "power1", stagger: 0.2})
+    timeline.from('#hero-section', 0.5 , {backgroundColor : "white"});
+    timeline.fromTo('.hero-image', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.hero-headline', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.hero-description', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    timeline.fromTo('.download-button', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+    gsap.fromTo('.copyright', 1, {opacity: "0%"}, {opacity: "100%"});
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     devServer : {
         contentBase : path.join(__dirname, "dist"),
         compress : true,
-        port : 9000,
+        port : 4444,
     },
     module:{
         rules:[
