c25dec5fbfaaa3727f7ec374c732cefdeccbd089
adityanegara <aditnegarabisnis@gmail.com>
2021-08-25 14:16:53 +0700

    Hero Component Done

---
+++ src/scripts/component/hero/HeroContainer.js
@@ -0,0 +1,27 @@
+class HeroSection extends HTMLElement {
+    connectedCallback(){
+        this.headline = this.getAttribute('headline') || null;
+        this.description = this.getAttribute('description') || null;
+        this.buttonCaption = this.getAttribute('button-caption') || null;
+        this.image = this.getAttribute('image') || null;
+        this.render();
+    };
+
+    render(){
+        this.innerHTML = `
+        <div class="hero-image">
+            <img src="${this.image}" alt="" />
+        </div>
+        <div class="hero-headline">
+            <h1>${this.headline}</h1>
+        </div>
+      <div class="hero-description">
+        <h2>${this.description}</h2>
+      </div>
+      <div class="download-button">
+        <button>${this.buttonCaption}</button>
+      </div>`
+    }
+}
+
+customElements.define('hero-section', HeroSection)
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,45 @@
 import "../styles/main.scss"
 import './component/navbar/navbar-list';
+import './component/hero/HeroContainer';
 import links from './component/navbar/navbar-links';
 import navSlide from './utils/navbar-animation';
+
+const linkAnimation = () =>{
+    const links = document.querySelectorAll('navbar-item');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            animateAboutPage();
+        });
+    });
+}
+
+const scrollToTop = () => {
+    window.scrollTo(0,0);
+}
 
 const renderNavbar = (navbarLinks) => {
     const navbarContainer = document.getElementById('nav-container');
     const navbarListElement = document.createElement('navbar-list');
     navbarListElement.navbarLinks = navbarLinks;
     navbarContainer.appendChild(navbarListElement);
+    linkAnimation();
 };
-  
-renderNavbar(links);
-navSlide();
 
-const nav = document.querySelector('navbar-list');
-const timeline = gsap.timeline();
-    // timeline.fromTo('#hero-section', 1 , {height : "0%"}, {height: "100%"});
+const animateAboutPage = () => {
+    scrollToTop();
+    const timeline = gsap.timeline();
     timeline.fromTo('#hero-section', 1 , {width : "0%"}, {width: "100%", ease: "power1"});
     timeline.fromTo('.hero-image', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
     timeline.fromTo('.hero-headline', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
     timeline.fromTo('.hero-description', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
     timeline.fromTo('.download-button', 0.5, { opacity : "0%"}, {opacity: "100%", ease:Power2.easeInOut});
+}
+
+
+renderNavbar(links);
+animateAboutPage();
+navSlide();
+
+
+  
+
