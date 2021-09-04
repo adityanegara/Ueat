4de4e88a97d76e8db98dfa19dd554dba8842ec2f
adityanegara <aditnegarabisnis@gmail.com>
2021-09-04 23:58:20 +0700

    Cache helper : There is a bug where user must offline online 2 time

--- src/scripts/serviceWorker.js
+++ src/scripts/serviceWorker.js
@@ -4,11 +4,13 @@ import CacheHelper from './utils/cache-helper';
 import serviceWorkerAssets from './globals/service-worker-assets';
 
 self.addEventListener('install', (event) => {
-  console.log(global.serviceWorkerOption);
+  console.log('install serviceworker');
+  console.log(serviceWorkerAssets);
   event.waitUntil(CacheHelper.cachingAppShell([...serviceWorkerAssets, './']));
 });
 
 self.addEventListener('activate', (event) => {
+  console.log('active serviceworker');
   event.waitUntil(CacheHelper.deleteOldCache());
 });
 
